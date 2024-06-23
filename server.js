0771317b6299ca8137403aa30765304e625995ea
alutterbein <alutterbein@gmail.com>
2024-06-23 13:03:32 -0700

    starting routes

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const uuid = require('./helpers/uuid');
 const PORT = 3001;
 const path = require('path');
 const app = express();
+const noteData = require('./db/db')
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static('public'));
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 );
+app.get('/api/notes', (req, res) => {
+    res.json(`${req.method} request received to get notes`);
+    console.info(`${req.method} request received to get notes`);
+});
 
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to add a note`)
